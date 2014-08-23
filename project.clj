8cfa307975da3a322cef7402c98cb32e50dbe554
marsmining <marsmining@gmail.com>
2014-08-23 22:10:54 +0200

    switch to clojure 1.7 for transducer action

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/marsmining/nettyca"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [ch.qos.logback/logback-classic "1.1.2"]
                  [io.netty/netty-handler "5.0.0.Alpha1"]
