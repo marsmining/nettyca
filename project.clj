eec88899dfcd61ac0e0a53450dd05a2cf78740dc
marsmining <marsmining@gmail.com>
2014-08-24 08:56:15 +0200

    make logback a dev dep

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [ch.qos.logback/logback-classic "1.1.2"]
+                 [org.clojure/tools.logging "0.3.0"]
                  [io.netty/netty-handler "5.0.0.Alpha1"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.5.8"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.5.8"]
+                                  [ch.qos.logback/logback-classic "1.1.2"]]}}
   :main nettyca.core)
