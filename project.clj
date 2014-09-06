a99a092ea161ce54786068fb7320bc445f61aa21
marsmining <marsmining@gmail.com>
2014-09-06 17:03:16 +0200

    update clojure to 1.7.0-alpha2

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/marsmining/nettyca"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
                  [org.clojure/tools.logging "0.3.0"]
                  [io.netty/netty-handler "5.0.0.Alpha1"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
